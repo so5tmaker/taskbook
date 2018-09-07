82ac5742fdd385d895957f02d930635a46d22f0a
Viktor Sivulskiy <svicvg@gmail.com>
2018-09-07 09:10:34 +0600

    The CSS Styles Were Deleted From The App.css File

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">TaskBook App</NavbarBrand>
