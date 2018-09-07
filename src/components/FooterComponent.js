9d76304e34b3cd70578b5d58f938049607e4e91a
Viktor Sivulskiy <svicvg@gmail.com>
2018-09-07 10:44:44 +0600

    The FooterComponent.js Was Added

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer(props) {
+    return(
+    <div className="footer">
+        <div className="container">
+            <div className="row justify-content-center">             
+                <div className="col-auto">
+                    <p>© Copyright 2018 TaskBook App</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Footer;
