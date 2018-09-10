685f3cd24adae0d22fdf8cb59fe6fd010fff7968
Viktor Sivulskiy <svicvg@gmail.com>
2018-09-10 07:39:22 +0600

    A Login Modal Was Added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,28 +1,86 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import {
+    Navbar, Nav, NavbarBrand, NavItem, Jumbotron,
+    Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label, Button
+} from 'reactstrap';
 
 class Header extends Component {
-  render() {
-    return(
-    <React.Fragment>
-      <Navbar dark>
-        <div className="container">
-            <NavbarBrand href="/">TaskBook App</NavbarBrand>
-        </div>
-      </Navbar>
-      <Jumbotron>
-           <div className="container">
-               <div className="row row-header">
-                   <div className="col-12 col-sm-6">
-                       <h1>Tasks for developers</h1>
-                       <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-                   </div>
-               </div>
-           </div>
-       </Jumbotron>
-    </React.Fragment>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value
+            + " Remember: " + this.remember.checked);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar dark>
+                    <div className="container">
+                        <NavbarBrand href="/">TaskBook App</NavbarBrand>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
+                            </NavItem>
+                        </Nav>
+                    </div>
+                </Navbar>
+                <Jumbotron>
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>Tasks for developers</h1>
+                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                        innerRef={(input) => this.remember = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Header;
